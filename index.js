7e68816d6345fa38237fcb04d03d6dbd766438f2
Scott Silverman <scottms724@gmail.com>
2018-02-19 17:00:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -85,14 +85,6 @@ function moveDodger(e) {
   if (e.which === RIGHT_ARROW) {
     moveDodgerRight();
   }
-  // implement me!
-  /**
-   * This function should call `moveDodgerLeft()`
-   * if the left arrow is pressed and `moveDodgerRight()`
-   * if the right arrow is pressed. (Check the constants
-   * we've declared for you above.)
-   * And be sure to use the functions declared below!
-   */
 }
 
 function moveDodgerLeft() {
