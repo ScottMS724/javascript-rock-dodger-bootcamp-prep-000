74dba1f32bd8c4c22a86845367f17dc7c335fdcc
Scott Silverman <scottms724@gmail.com>
2018-02-19 17:23:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,16 +75,15 @@ function endGame() {
 }
 
 function moveDodger(e) {
-   if(e.which === LEFT_ARROW){
-     moveDodgerLeft();
-   e.preventDefault();
-   e.stopPropagation();
-   } else if(e.which === RIGHT_ARROW) {
-     moveDodgerRight();
-     e.preventDefault();
-     e.stopPropagation();
-     }
-   }
+  if([LEFT_ARROW, RIGHT_ARROW].indexOf(e.which) > -1){
+    e.preventDefault();
+    e.stopPropagation();
+  }
+  if (e.which === LEFT_ARROW){
+    moveDodgerLeft()
+  }if (e.which === RIGHT_ARROW){
+    moveDodgerRight()
+  }}
 
 function moveDodgerLeft() {
   window.addEventListener(function() {
